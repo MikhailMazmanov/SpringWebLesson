ff3aab1100434c0be7eaaf974f35d255bbe23d96
MikhailMazmanov <mihailmazmanov@gmail.com>
2025-07-02 16:56:03 +0300

    Initial commit

---
+++ src/main/java/org/example/springweblesson/SpringWebLessonApplication.java
@@ -0,0 +1,13 @@
+package org.example.springweblesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebLessonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springweblesson/SpringWebLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springweblesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebLessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
