558ca6e2ce4449390d9f8e30b386fe0c0d3f4f5b
Ilya278-ai <pramestatus@gmail.com>
2025-07-11 20:53:17 +0700

    add pages for array (error on pages) and methods and while and for

--- src/main/java/org/example/springweblesson/InfoController.java
+++ src/main/java/org/example/springweblesson/InfoController.java
@@ -44,9 +44,26 @@ public class InfoController {
     @GetMapping("/java-var")
     public String javaVar(){return "java-var";}
 
-    @GetMapping("/string-class-metods-java")
+    @GetMapping("/stri ng-class-metods-java")
     public String strClassMetodsWeb(){return "string-class-metods-java";}
 
     @GetMapping("/if-java")
     public String ifJavaWeb(){return "if-java";}
+
+    @GetMapping("/info-arrays")
+    public String infoArrays(){return "/info-arrays";}
+
+    @GetMapping("/info-cycle")
+    public String infoCycle(){return "/info-cycle";}
+
+    @GetMapping("/info-methods")
+    public String infoMethods(){return "/info-methods";}
+
+    @GetMapping("/info-collections")
+    public String infoCollections(){return "info-collections";}
+
+
+    @GetMapping("info-java-list-set-map")
+    public String infoListSetMap(){return "info-java-list-set-map";}
+
 }
